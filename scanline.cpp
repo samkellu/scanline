4bf7f2cf4302e10ba71f4a1a9755e53c9dd611f7
sam <skel4720@uni.sydney.edu.au>
2025-01-04 12:37:28 +1100

    hu:

--- scanline.cpp
+++ scanline.cpp
@@ -4,6 +4,9 @@
 
 int main()
 {
+    SDL_Window* window;
+    SDL_Event event;
+    SDL_Renderer* renderer;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) 
     {
@@ -11,12 +14,10 @@ int main()
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Scanline", 
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SCR_WIDTH,
-                                          SCR_HEIGHT,
-                                          0);
+    window = SDL_CreateWindow("Scanline", 
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              SCR_WIDTH, SCR_HEIGHT, 0);
 
     if (!window)
     {
@@ -24,29 +25,26 @@ int main()
         return -1;
     }
 
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    if (!surface)
+    renderer = SDL_CreateRenderer(window, 0, 0);
+    if (!renderer)
     {
-        std::cout << "Failed to extract surface: " << SDL_GetError();
-        return -1; 
+        std::cout << "Failed to create renderer: " << SDL_GetError();
+        return -1;
     }
 
-    bool running = true;
-    while (running)
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+    int i = 0;
+    while (1)
     {
-        SDL_Event e;
-        while (SDL_PollEvent(&e) > 0)
-        {
-            switch (e.type)
-            {
-                case SDL_QUIT:
-                    running = false;
-                    break;
-            }
-            
-            SDL_UpdateWindowSurface(window);
-        }
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT) break;
+
+        SDL_RenderDrawPoint(renderer, i % SCR_WIDTH, i / SCR_WIDTH);
+        SDL_RenderPresent(renderer);
+        i++;
+        SDL_Delay(1);
     }   
 
-    return 1;
+    return 0;
 }
